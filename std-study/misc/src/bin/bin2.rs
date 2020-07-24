9b2d6d2045c51a4993014c6de68c21d7111a482b
r31oad3d <r31oad3d@163.com>
2020-07-25 00:31:42 +0800

    20200724

---
+++ std-study/misc/src/bin/bin2.rs
@@ -0,0 +1,34 @@
+use itertools::{Itertools, Merge, MergeBy};
+
+fn main() {
+    println!("{:?}", (1..=10).filter(|x| x % 2 == 0).size_hint());
+    println!("{:?}", (1..=7).zip(1..=6).collect::<Vec<_>>());
+
+    let ret1 = ["1", "2", "a"]
+        .iter()
+        .map(|xx| xx.parse::<i32>())
+        .inspect(|num| {
+            if let Err(ref e) = *num {
+                println!("error: {}", e)
+            }
+        })
+        .filter_map(|ret| ret.ok())
+        // .filter(|ret| ret.is_ok())
+        // .map(|ret| ret.unwrap())
+        .sum::<i32>();
+    println!("ret1:{:?}", ret1);
+
+    for elt in itertools::merge(&[1, 2, 3], &[2, 3, 4]) {
+        println!("{:?}", elt);
+    }
+    let mut merge1 = itertools::merge(&[1, 2, 3], &[2, 3, 4]);
+    println!("merge1:{:?}", merge1.clone().sum::<i32>());
+    println!(
+        "merge1:{:?}",
+        merge1.clone().map(|x| x * x).collect::<Vec<_>>()
+    );
+
+    //let ret2 = (&merge1 as &mut dyn Iterator<Item=&i32>).count();
+    let ret2 = merge1.product::<i32>();
+    println!("ret2:{:?}", ret2);
+}
--- std-study/misc/src/bin/functional.rs
+++ std-study/misc/src/bin/functional.rs
@@ -1,3 +1,81 @@
-fn main() {
+struct Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
 
+impl<T> Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cache<T> {
+        Cache {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let add_v3 = |x: i32| x + 1;
+    let add_v4 = |x: i32| x + 1;
+
+    let example_1 = move |x: i32| x;
+
+    let v1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("{:?}", v1.iter().sum::<i32>());
+    println!(
+        "{:?}",
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            .iter()
+            .filter(|x| *x % 2 == 0)
+            .sum::<i32>()
+    );
+    ['a', 'b', 'c', 'd']
+        .iter()
+        .for_each(|char| println!("{:?}", char));
+    let mut cnt = Counter::new();
+    println!("{:?}", cnt.next());
+    let sum = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum::<u32>();
+    println!("{:?}", sum);
 }
