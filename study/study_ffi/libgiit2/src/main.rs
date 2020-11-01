288af9af608f7c554ade572300b440bccf590115
r31oad3d <r31oad3d@163.com>
2020-11-01 22:35:13 +0800

    20201021

---
+++ study/study_ffi/libgiit2/src/main.rs
@@ -0,0 +1,12 @@
+use std::os::raw::c_int;
+#[link(name = "git2")]
+extern "C" {
+    pub fn git_libgit2_init() -> c_int;
+    pub fn git_libgit2_shutdown() -> c_int;
+}
+fn main() {
+    unsafe {
+        git_libgit2_init();
+        git_libgit2_shutdown();
+    }
+}
